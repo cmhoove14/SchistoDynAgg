3ebe7c5a440e2a1bdaff3963ea69bd0527e8a8df
Chris Hoover <cmhoove14@gmail.com>
2021-03-02 17:30:34 -0800

    ABC Run scripts

---
+++ Analysis/21-ABC-Worm-Burden-Estimation.R
@@ -0,0 +1,100 @@
+# -----------------------------
+# SchistoDynAgg ABC worm burden estimation from egg burden
+# Chris Hoover
+# -----------------------------
+
+
+# Setup ---------------
+  devtools::load_all()
+  
+
+# Load data and process for input to ABC ---------------
+  adlt_sums <- readRDS(here::here("Data", "Derived", "adults_shehia_sums_Unguja_Pemba_2012_2017.rds"))
+  chld_sums <- readRDS(here::here("Data", "Derived", "chldrn_shehia_sums_Unguja_Pemba_2012_2017.rds"))
+  
+  
+  yOc <- chld_sums %>%
+    filter(UF_max > 1 & Year > 2011) %>%
+    mutate(
+      UF_se = sqrt(UF_var) / sqrt(n_ppl),
+      UFpos2n = UF_pos ^ 2 / n_ppl,
+      pop = "Child"
+    ) %>%
+    ungroup() %>%
+    dplyr::select(Isl, Shehia, Year, UF_mean, UF_se, UF_pos, n_ppl, UFpos2n, pop)
+  
+  
+  
+  yOa <- adlt_sums %>%
+    filter(UF_max > 1 & Year > 2011) %>%
+    mutate(
+      UF_se = sqrt(UF_var) / sqrt(n_ppl),
+      UFpos2n = UF_pos ^ 2 / n_ppl,
+      pop = "Adult"
+    ) %>%
+    ungroup() %>%
+    dplyr::select(Isl, Shehia, Year, UF_mean, UF_se, UF_pos, n_ppl, UFpos2n, pop)
+  
+  
+  
+  yO <- bind_rows(yOc, yOa)
+  
+# Declare priors and run in parallel. takes ~ 3 hours on 8 core Lenovo ThinkPad with AMD Ryzen processor or ~1.5 hours on 24 compute node on Biostat cluster with 24 cores ----------------------
+  abc_priors <- list(
+    "mean_W_lo"     = 1e-6,
+    "mean_W_hi"     = 1e3,
+    "disp_W_lo"     = 1e-5,
+    "disp_W_hi"     = 1e5,
+    "susc_shape_lo" = 1,
+    "susc_shape_hi" = 1,
+    "susc_rate_lo"  = 1 / 500,
+    "susc_rate_hi"  = 1 / 500,
+    "mean_C_lo"     = 1,
+    "mean_C_hi"     = 1e6,
+    "disp_C_lo"     = 1e-5,
+    "disp_C_hi"     = 1e5,
+    "h_lo"          = 10,
+    "h_hi"          = 10,
+    "r_lo"          = 1,
+    "r_hi"          = 1,
+    "g_lo"          = 0.001,
+    "g_hi"          = 0.001
+  )
+  
+#Setup for running jobs across parallel nodes in cluster
+  n_iter <- 1e5
+  
+  start.time <- Sys.time()
+  n_cores <- parallel::detectCores()
+  cl <- parallel::makeCluster(n_cores)
+  registerDoParallel(cl)
+  
+  clusterEvalQ(cl, devtools::load_all())
+  
+  abc_sims <- foreach(
+    x = 1:nrow(yO),
+    .packages = c("tidyverse", "abc"),
+    .verbose = TRUE,
+    .options.RNG = 7491
+  ) %dorng% {
+    # See R/abc_worm_estimation_functions for abc_fit and other functions
+    abc_fit(obs_data = yO[x, ],
+            priors = abc_priors,
+            iterations = n_iter)
+  }
+  
+  parallel::stopCluster(cl)
+  
+  end.time <- Sys.time()
+  
+  end.time - start.time
+  
+  saveRDS(
+    abc_sims,
+    file = here::here("Data/Derived", 
+                      paste0(
+                        "abc_fit_ridge_log_4cases_hrg_fixed_",
+                        n_iter,
+                        "iterations.rds"
+                      ))
+  )    
