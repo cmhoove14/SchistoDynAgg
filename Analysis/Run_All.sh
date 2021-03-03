3642bae6a7cef72649fde0a4c405f791930ae329
Chris Hoover <cmhoove14@gmail.com>
2021-03-02 17:31:01 -0800

    Add ABC to run all

--- Analysis/Run_All.sh
+++ Analysis/Run_All.sh
@@ -8,6 +8,8 @@
 #$ -m beas
 # 
 Rscript Analysis/0-Data-Process.R  
-Rscript Analysis/11-Agg-Burden_GEE.R 
-Rscript Analysis/12-kappa_change_bootstrap.R
-Rscript Analysis/1F-Fig1-AggGEE-Results.R
+Rscript Analysis/11-Agg-Burden_GEE.R  
+Rscript Analysis/12-kappa_change_bootstrap.R  
+Rscript Analysis/1F-Fig1-AggGEE-Results.R  
+Rscript Analysis/21-ABC-Worm-Burden-Estimation.R  
+Rscript Analysis/22-ABC_Sims-PostProcess.R  
