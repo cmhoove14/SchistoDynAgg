22331ad82e1a05f8d6506f2d1753c14545ff36e1
Chris Hoover <cmhoove14@gmail.com>
2021-03-02 16:59:05 -0800

    bash to runall analysis scripts

---
+++ Analysis/Run_All.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+# 
+#$ -cwd 
+#$ -V 
+#$ -j y 
+#$ -S /bin/bash 
+#$ -M choover@berkeley.edu
+#$ -m beas
+# 
+Rscript Analysis/0-Data-Process.R  
+Rscript Analysis/11-Agg-Burden_GEE.R 
+Rscript Analysis/12-kappa_change_bootstrap.R
+Rscript Analysis/1F-Fig1-AggGEE-Results.R
