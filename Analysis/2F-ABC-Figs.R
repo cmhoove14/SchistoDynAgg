ddea8cde45d3c0473cabdeb853851a309c377c51
cmhoove14 <cmhoove14@gmail.com>
2021-03-11 12:26:32 -0800

    Add GEE to worm burden/aggregation rather than loess smooth

--- Analysis/2F-ABC-Figs.R
+++ Analysis/2F-ABC-Figs.R
@@ -9,16 +9,29 @@ devtools::load_all()
 load(here::here("Data/Derived/abc_processed_results.Rdata"))
 
 # Case1 PLot ----------------------
+case1_gee <- geeglm(log(obsalphaW.med) ~ log(obsW.med), id = as.factor(Shehia),
+                    family = "gaussian", corstr = "unstructured",
+                    weights = 1/(obsalphaW.hiq-obsalphaW.loq),
+                    data = abc_fin_df_case_long  %>% filter(Case=="Case1" & pop == "Comm"))
+
+case1_gee_coef1 <- coef(case1_gee)[1]
+case1_gee_coef2 <- coef(case1_gee)[2]
+
+case1_gee_fx <- function(W){
+  exp(case1_gee_coef1+case1_gee_coef2*log(W))^-1
+}
+
+
 kap_W_case1_plot <- abc_fin_df_case_long %>%
   filter(Case=="Case1" & pop == "Comm") %>% 
   ggplot(aes(x = obsW.med,
-             y = obsalphaW.med^-1, 
-             weight = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq))) +
-  geom_point(aes(size = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq)),
+             y = obsalphaW.med^-1)) +
+  geom_point(aes(size = 1/(obsalphaW.hiq-obsalphaW.loq)),
              col = "#058dd6",
              alpha = 0.3,
              show.legend = FALSE) +
-  stat_smooth(col = "black") +
+  #stat_smooth(col = "black") +
+  stat_function(fun = case1_gee_fx, size = 1.2, col = "black") +
   theme_classic() +
   theme(axis.title.x = element_blank(),
         plot.title = element_text(hjust = 0.1, vjust = -1)) +
@@ -36,16 +49,29 @@ kap_W_case1_plot <- abc_fin_df_case_long %>%
        title = "A")
 
 # Case2 PLot ----------------------
+case2_gee <- geeglm(log(obsalphaW.med) ~ log(obsW.med), id = as.factor(Shehia),
+                    family = "gaussian", corstr = "unstructured",
+                    weights = 1/(obsalphaW.hiq-obsalphaW.loq),
+                    data = abc_fin_df_case_long  %>% filter(Case=="Case2" & pop == "Comm"))
+
+case2_gee_coef1 <- coef(case2_gee)[1]
+case2_gee_coef2 <- coef(case2_gee)[2]
+
+case2_gee_fx <- function(W){
+  exp(case2_gee_coef1+case2_gee_coef2*log(W))^-1
+}
+
+
 kap_W_case2_plot <- abc_fin_df_case_long %>%
   filter(Case=="Case2" & pop == "Comm") %>% 
   ggplot(aes(x = obsW.med,
-             y = obsalphaW.med^-1, 
-             weight = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq))) +
-  geom_point(aes(size = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq)),
+             y = obsalphaW.med^-1)) +
+  geom_point(aes(size = 1/(obsalphaW.hiq-obsalphaW.loq)),
              col = "#cc4a49",
              alpha = 0.3,
              show.legend = FALSE) +
-  stat_smooth(col = "black") +
+  #stat_smooth(col = "black") +
+  stat_function(fun = case2_gee_fx, size = 1.2, col = "black") +
   theme_classic() +
   theme(axis.text.y = element_blank(),
         axis.title = element_blank(),
@@ -65,16 +91,28 @@ kap_W_case2_plot <- abc_fin_df_case_long %>%
   labs(title = "B")
 
 # Case3 PLot ----------------------
+case3_gee <- geeglm(log(obsalphaW.med) ~ log(obsW.med), id = as.factor(Shehia),
+                    family = "gaussian", corstr = "unstructured",
+                    weights = 1/(obsalphaW.hiq-obsalphaW.loq),
+                    data = abc_fin_df_case_long  %>% filter(Case=="Case3" & pop == "Comm"))
+
+case3_gee_coef1 <- coef(case3_gee)[1]
+case3_gee_coef2 <- coef(case3_gee)[2]
+
+case3_gee_fx <- function(W){
+  exp(case3_gee_coef1+case3_gee_coef2*log(W))^-1
+}
+
 kap_W_case3_plot <- abc_fin_df_case_long %>%
   filter(Case=="Case3" & pop == "Comm") %>% 
   ggplot(aes(x = obsW.med,
-             y = obsalphaW.med^-1, 
-             weight = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq))) +
-  geom_point(aes(size = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq)),
+             y = obsalphaW.med^-1)) +
+  geom_point(aes(size = 1/(obsalphaW.hiq-obsalphaW.loq)),
              col = "#7b6c7c",
              alpha = 0.3,
              show.legend = FALSE) +
-  stat_smooth(col = "black") +
+  #stat_smooth(col = "black") +
+  stat_function(fun = case3_gee_fx, size = 1.2, col = "black") +
   theme_classic() +
   theme(plot.title = element_text(hjust = 0.1, vjust = -1)) +
   scale_size(range = c(1,4)) +
@@ -92,16 +130,28 @@ kap_W_case3_plot <- abc_fin_df_case_long %>%
        title = "C")
 
 # Case4 PLot ----------------------
+case4_gee <- geeglm(log(obsalphaW.med) ~ log(obsW.med), id = as.factor(Shehia),
+                    family = "gaussian", corstr = "unstructured",
+                    weights = 1/(obsalphaW.hiq-obsalphaW.loq),
+                    data = abc_fin_df_case_long  %>% filter(Case=="Case4" & pop == "Comm"))
+
+case4_gee_coef1 <- coef(case4_gee)[1]
+case4_gee_coef2 <- coef(case4_gee)[2]
+
+case4_gee_fx <- function(W){
+  exp(case4_gee_coef1+case4_gee_coef2*log(W))^-1
+}
+
 kap_W_case4_plot <- abc_fin_df_case_long %>%
   filter(Case=="Case4" & pop == "Comm") %>% 
   ggplot(aes(x = obsW.med,
-             y = obsalphaW.med^-1, 
-             weight = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq))) +
-  geom_point(aes(size = obsalphaW.med/(obsalphaW.hiq-obsalphaW.loq)),
+             y = obsalphaW.med^-1)) +
+  geom_point(aes(size = 1/(obsalphaW.hiq-obsalphaW.loq)),
              col = "#64a860",
              alpha = 0.3,
              show.legend = FALSE) +
-  stat_smooth(col = "black") +
+  #stat_smooth(col = "black") +
+  stat_function(fun = case4_gee_fx, size = 1.2, col = "black") +
   theme_classic() +
   theme(axis.text.y = element_blank(),
         axis.title.y = element_blank(),
