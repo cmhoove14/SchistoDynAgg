29d04883ecc2226e8d7844ca381d218767a0ad28
cmhoove14 <cmhoove14@gmail.com>
2021-03-11 12:26:03 -0800

    Move partition parameter to own script

--- Analysis/2F2-ABC-Mod-Comp-BayesF.R
+++ Analysis/2F2-ABC-Mod-Comp-BayesF.R
@@ -8,35 +8,6 @@ devtools::load_all()
 
 load(here::here("Data/Derived/abc_processed_results.Rdata"))
 
-# Case 4 partition parameter summary ----------------
-Case4_part_par_by_W <- abc_fin_df_case_long %>%
-  filter(Case=="Case4" & pop == "Comm") %>% 
-  ggplot(aes(x = obsW.med,
-             y = partW.med, 
-             weight = partW.med/(partW.hiq-partW.loq))) +
-  geom_point(aes(size = partW.med/(partW.hiq-partW.loq)),
-             col = "#64a860",
-             alpha = 0.3,
-             show.legend = FALSE) +
-  stat_smooth(col = "black") +
-  theme_classic() +
-  theme(axis.title.x = element_blank(),
-        plot.title = element_text(hjust = 0.1, vjust = -1)) +
-  scale_size(range = c(1,4)) +
-  scale_x_continuous(trans = "log",
-                     breaks = c(0.01, 0.1, 1, 10, 100),
-                     labels = c("0.01", "0.1", "1", "10", "100"),
-                     limits = c(min(abc_fin_df_case_long$obsW.med, na.rm = T),
-                                max(abc_fin_df_case_long$obsW.med, na.rm = T))) +
-  ylim(c(0,1)) +
-  labs(y = "Case 4 partition parameter",
-       x = "W")
-
-Case4_part_par_by_W
-
-ggsave(here::here("Figures/ABC_Case4_Partition_Parameter_by_W.png"),
-       height = 4, width = 4, units = "in")
-
 # Bayes factors (Comparative estimate of model performance) for each case across observed egg burden -------------
 abc_bayesF_comp <- abc_fin_df_case_long %>% 
   filter(Case %in% c("IIItoI", "IIItoII", "IItoI",
