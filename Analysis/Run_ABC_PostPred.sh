0e77090bfe30ff8d81302772421d0d5b4615f02a
cmhoove14 <cmhoove14@gmail.com>
2021-03-12 12:40:43 -0800

    Just run posterior prediction script

---
+++ Analysis/Run_ABC_PostPred.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+# 
+#$ -cwd 
+#$ -V 
+#$ -j y 
+#$ -S /bin/bash 
+#$ -M choover@berkeley.edu
+#$ -m beas
+# 
+# Rscript Analysis/0-Data-Process.R  
+# Rscript Analysis/21-ABC-Worm-Burden-Estimation.R  
+# Rscript Analysis/22-ABC-Sims-PostProcess.R  
+Rscript Analysis/23-ABC-PostPred-Checks.R  
